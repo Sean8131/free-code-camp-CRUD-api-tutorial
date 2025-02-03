0dc3a4a85fed25b0b1f85967010fde1a8ee15819
Sean Selby <seanselbyy@gmail.com>
2025-02-04 11:03:39 +1300

    Add nodemon Add get request

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import bodyParser from ‘body-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('[GET ROUTE');
+    res.send('Hello from homepage');
+})
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
